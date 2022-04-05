f4e57030cc8390755ffac677bfc09f022896f05f
Ishan Naithani <ishan_naithani@intuit.com>
2022-04-05 14:37:05 +0800

    generate bash

---
+++ generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "RUNNING generate.sh script"
+
+echo "Creating file schema.json"
+echo "{name: 'test'}" > schema.json
+
+echo "Creating file schema.js"
+echo "console.log('test file generated')" > schema.js
+
+echo "----xxxxx---- Completed file generation ----xxxxx----"
