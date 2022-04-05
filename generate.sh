bf216299f25a911226796cc5debc342a5fb378b5
Ishan Naithani <ishan_naithani@intuit.com>
2022-04-05 18:22:24 +0800

    w

--- generate.sh
+++ generate.sh
@@ -28,7 +28,7 @@ git status
 git push origin $BRANCH_NAME
 
 curl --max-time 120 -X POST \
-                      -u "${GITHUB_USER}:${GITHUB_TOKEN}"
+                      -u "${GITHUB_USER}:${GITHUB_TOKEN}" \
                        -H "Accept: application/vnd.github.v3+json" \
                       "https://api.github.com/repos/ishannaithani/jenkins-test/pulls" \
                       -d '{
